a8ff2649c58f0277a6543a24d945ca2baee6aa25
Nicoo200 <nicollass049@gmail.com>
2025-06-16 15:58:36 -0300

    codigo arrumado

---
+++ src/service/AuthApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://clinicapi-h4bfgdfac8bcfpbt.brazilsouth-01.azurewebsites.net/auth';
+
+export async function loginUsuario(credentials) {
+  const response = await axios.post(`${BASE_URL}/login`, credentials);
+  return response.data;
+}
+
+export async function cadastrarUsuario(dados) {
+  const response = await axios.post(`${BASE_URL}/users`, dados);
+  return response.data;
+}
---
+++ src/service/ConsultaApi.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://clinicapi-h4bfgdfac8bcfpbt.brazilsouth-01.azurewebsites.net';
+
+export async function getConsultas() {
+  try {
+    const response = await axios.get(`${BASE_URL}/appointments`);
+    const data = response.data;
+
+    if (Array.isArray(data.dados)) {
+      return data.dados;
+    } else {
+      console.warn('Formato inesperado na resposta:', data);
+      return [];
+    }
+  } catch (error) {
+    console.error('Erro ao buscar consultas:', error);
+    return [];
+  }
+}
+
+export async function createConsulta(consulta) {
+  try {
+    const response = await axios.post(`${BASE_URL}/appointments`, consulta);
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      console.error('Erro ao criar consulta:', error.response.data);
+    } else {
+      console.error('Erro desconhecido:', error.message);
+    }
+    throw error;
+  }
+}
+
+export async function deleteConsulta(id) {
+  try {
+    const response = await axios.delete(`${BASE_URL}/appointments/${id}`);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      console.error('Erro ao deletar consulta:', error.response.data);
+    } else {
+      console.error('Erro desconhecido ao deletar consulta:', error.message);
+    }
+    throw error;
+  }
+}
